cf4c196ede17df2a8085657c8e138c230b0c5332
mac <415677303@qq.com>
2019-10-26 11:31:13 +0800

    add native bayes

--- statistics/native_bayes/bayes_main.py
+++ statistics/native_bayes/bayes_main.py
@@ -13,6 +13,6 @@ data_list = read_data[["颜色", "容量", "品牌", "价格", "能不能买"]]
 # search = {"颜色": "蓝色", "容量": "1",
 #           "品牌": "RoyalCopenhagen", "价格": 50}
 
-search = ["白色", "1", "Starbucks", 420]
+search = ["蓝色", "2", "Starbucks", 420]
 
 bayes.bayes(np.asarray(data_list), -1, search, 1)
